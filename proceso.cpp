83ce17ceb60a448b18f33d7e1088d477e886a4ce
laurasgm <laurasophia.gonzalez@utp.edu.co>
2019-09-13 18:16:53 -0500

    fork with vfork

--- proceso.cpp
+++ proceso.cpp
@@ -8,7 +8,7 @@
 #include <sys/shm.h>
 //https://www.geeksforgeeks.org/fork-system-call/
 //https://timmurphy.org/2014/04/26/using-fork-in-cc-a-minimum-working-example/
-
+//el vfork si comparte memoria, el fork no
 using namespace std;
 
 //double **newGrid = (double **) mmap(NULL, ARRAY_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_SHARED, -1, 0);
@@ -57,7 +57,7 @@ int main(int argc, char **argv)
           *(*(m2+i)+j) =rand() % 10 + 1;
       }
   }
-
+/*
   //imprimir
   for (int i=0; i<TAM; i++){
 
@@ -76,42 +76,54 @@ int main(int argc, char **argv)
       }
       cout<<endl;
   }
-
+*/
   int pepito=0;
+  int cont=0;
   ScopedTimer p;
   //recuerda que el fork funciona como 2 ^n entonces 2 ^5 nos saca 32 datos o entradas a proceso hijo
-    for (int i=0; i<TAM; i++){
-    pid = fork();
-    //cout<<"este es pid: "<<pid<<endl;
-    if (pid<0){
-      cout<<"error, el pid es menor a cero";
-    }
-    //child
-    if (pid==0){
-      //cout<<"entre"<<endl;
-      for(int i=0; i<TAM; i++){
-
-        *(*(r+pepito)+i) += *(*(m1+pepito)+i) * *(*(m2+i)+pepito);
-        //cout<<"soy m1: "<<*(*(m1+i)+j)<<endl;
-        //cout<<"soy m2: "<<*(*(m2+i)+j)<<endl;
-        cout<<"soy r: "<<*(*(r+pepito)+i)<<endl;
+  for (int i=0; i<TAM; i++){
+      pid = vfork();
+      //cout<<"este es pid: "<<pid<<endl;
+      if (pid<0){
+        cout<<"error, el pid es menor a cero";
       }
-      exit(0);
+      //child
+      if (pid==0){
+        //cout<<"entre"<<endl;
+        for(int i=0; i<TAM; i++){
+          for(int j=0; j<TAM; j++){
+            //*(*(r+pepito)+cont) += *(*(m1+pepito)+i) * *(*(m2+i)+pepito);
+            *(*(r+pepito)+i) += *(*(m1+pepito)+j) * *(*(m2+j)+i);
+            //cout<<"soy m1: "<<*(*(m1+pepito)+j)<<endl;
+            //cout<<"soy m2: "<<*(*(m2+j)+i)<<endl;
+            //cout<<"soy r: "<<*(*(r+pepito)+i)<<endl;
+
+            //cout<<"soy m1: "<<*(*(m1+pepito)+i)<<endl;
+            //cout<<"soy m2: "<<*(*(m2+i)+pepito)<<endl;
+            //cout<<"soy r: "<<*(*(r+pepito)+cont)<<endl;
+            //cout<<"soy i: "<<i<<endl;
+          }
+        }
+        exit(0);
+      }
+
+      //father
+      if (pid>0){
+        //cout<<"soy cont"<<cont<<endl;
+        //cout<<"soy pepito"<<pepito<<endl;
+        //cont+=1;
+        pepito+=1;
+        wait(0);
+      }
     }
 
-    //father
-    if (pid>0){
-      pepito+=1;
-    }
-  }
-  wait(0);
 
   //cout<<"hola soy pepito "<<pepito<<endl;
   cout<<TAM<<","<<p.elapsed()/1e+6<<endl;
-  shmdt(r);
-  shmctl(id,IPC_RMID,NULL);
+  //shmdt(r);
+  //shmctl(id,IPC_RMID,NULL);
   //}
-
+/*
   cout<<endl
       << "Multiplicacion fork" << endl;
   for (int i=0; i<TAM; i++){
@@ -120,5 +132,6 @@ int main(int argc, char **argv)
       }
       cout<<endl;
   }
+*/
   return 0;
 }
